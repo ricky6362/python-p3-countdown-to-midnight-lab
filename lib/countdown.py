0ffff36c1ac805fef9b2e8a6f12842dd431bae0a
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-08-21 11:35:59 +0300

    Assignment done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,9 @@
 # your code goes here!
+def countdown(number):
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        if number == 0:
+            print("HAPPY NEW YEAR!")
+
+countdown(30)
